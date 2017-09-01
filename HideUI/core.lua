5af1f70ff9cd54c165e167c6e0e2092e159fe5d2
JakiraElysium <31551410+JakiraElysium@users.noreply.github.com>
2017-09-01 16:15:16 -0400

    Add files via upload

---
+++ HideUI/core.lua
@@ -0,0 +1,10 @@
+-- Created by Jakira of Elysium Project v0.1
+
+local Frame = CreateFrame("Frame")
+Frame:RegisterEvent("PLAYER_LOGIN")
+
+Frame:SetScript("OnEvent", function(...)
+MainMenuBarLeftEndCap:Hide()
+MainMenuBarRightEndCap:Hide()
+
+end)
